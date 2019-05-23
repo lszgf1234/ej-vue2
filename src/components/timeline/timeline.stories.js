7b0f35a772cfd973687bd16cdac280784c9b5d3b
SilentDepth <silentdepth@outlook.com>
2019-05-23 18:37:35 +0800

    补充 story

--- src/components/timeline/timeline.stories.js
+++ src/components/timeline/timeline.stories.js
@@ -1,6 +1,8 @@
 import {storiesOf} from '@storybook/vue'
 import Vue from 'vue'
 
+import tailwindConfig from '../../../tailwind.config'
+import {cloneDeep} from '../../utils'
 import Timeline from '.'
 
 Vue.use(Timeline)
@@ -8,36 +10,49 @@ Vue.use(Timeline)
 const data = [
   {
     timestamp: '2018-04-12 20:46',
-    color: 'blue',
-    title: 'ls进行了一步操作',
-    desc: '这是什么操作',
+    title: '创建成功',
+  },
+  {
+    timestamp: '2018-04-13T12:23:34.567',
+    title: '通过审核',
+    desc: '不知道为什么但就是通过了',
+  },
+  {
+    timestamp: '2018-04-14',
+    title: '活动进行中',
     files: [
       {
-        href: '/',
-        name: '文件1.zip',
-        type: 2,
-        size: '1M',
-      },
-      {
-        href: '/',
-        name: '文件2.docx',
-        type: 1,
-        size: '100KB',
-      },
-      {
-        href: 'http://jianjiaoedu.bj.bcebos.com/201807091159335b42dda567baf.jpg',
-        name: '文件2.png',
+        href: '/file1',
+        name: '活动照片1.jpg',
         type: 3,
-        size: '10KB',
+        size: '123KB',
+      },
+      {
+        href: '/file2',
+        name: '活动照片2.jpeg',
+        type: 3,
+        size: '42KB',
+      },
+      {
+        href: '/file3',
+        name: '节目表.pdf',
+        type: 1,
+        size: '234KB',
       },
     ],
   },
   {
-    timestamp: '2018-04-12 20:46',
-    color: 'red',
-    title: 'ls又进行了一步操作',
-    desc: '',
-    files: [],
+    timestamp: '2018-04-15 00:00:00',
+    title: '活动结束',
+    desc: '活动圆满结束',
+    files: [
+      {
+        href: '/file4',
+        name: '活动纪要和录像.zip',
+        type: 2,
+        size: '20.4MB',
+      },
+    ],
   },
 ]
 
@@ -49,6 +64,20 @@ storiesOf('Timeline', module)
     props: {
       data: {
         default: data,
+      },
+    },
+  }))
+  .add('自定义节点样式', () => ({
+    template: `
+      <ej-timeline :data="data"/>
+    `,
+    props: {
+      data: {
+        default: () => {
+          const clone = cloneDeep(data)
+          clone[3].color = tailwindConfig.theme.extend.colors.green.default
+          return clone
+        },
       },
     },
   }))
---
+++ src/components/timeline/timeline.vue
@@ -0,0 +1,162 @@
+<template>
+  <div class="timeline-view">
+    <el-timeline>
+      <el-timeline-item
+        v-for="(item, key) in data"
+        :key="key"
+        :color="item.color"
+        :timestamp="item.timestamp"
+        placement="top"
+      >
+        <div>
+          <div class="title" v-if="!hasMore(item)">
+            <span class="text">{{item.title}}</span>
+          </div>
+          <div :class="{active: dataStatus[key]}" v-else>
+            <div class="title mb-sm">
+              <a class="text " href="javascript:" @click="show(item, key)">
+                <span>{{item.title}}</span>
+                <i class="el-icon-arrow-down icon"></i>
+              </a>
+            </div>
+            <transition @before-enter="filterBeforeEnter" @enter="filterEnter" @leave="filterLeave">
+              <div v-show="dataStatus[key]" class="more-box">
+                <div class="desc mb-xs">{{item.desc}}</div>
+                <div class="mb-xs" v-for="(itemChild, index) in (item.files || [])" :key="index">
+                  <a class="download" :href="itemChild.href">
+                    <ej-icon :icon="getIcon(itemChild.type)" class="icon-file"/>
+                    <span class="text">{{itemChild.name}}</span>
+                    <span class="size">{{itemChild.size}}</span>
+                  </a>
+                </div>
+              </div>
+            </transition>
+          </div>
+        </div>
+      </el-timeline-item>
+    </el-timeline>
+  </div>
+</template>
+
+<script>
+  import Vue from 'vue'
+  import {Timeline, TimelineItem} from 'element-ui'
+
+  import Icon from '../icon'
+
+  const typeList = [
+    {value: 1, text: '文档', icon: 'textfile'},
+    {value: 2, text: '压缩文件', icon: 'folder'},
+    {value: 3, text: '图片', icon: 'image'},
+  ]
+
+  export default {
+    name: 'EjTimeline',
+    components: {
+      [Timeline.name]: Timeline,
+      [TimelineItem.name]: TimelineItem,
+      [Icon.name]: Icon,
+    },
+    props: {
+      data: {
+        type: Array,
+        default: () => []
+      },
+    },
+    data () {
+      return {
+        dataStatus: [],
+      }
+    },
+    watch: {
+      data () {
+        this.initStatus()
+      }
+    },
+    created () {
+      this.initStatus()
+    },
+    methods: {
+      hasMore (item) {
+        return (item.desc || (item.files || []).length)
+      },
+      show (item, index) {
+        this.$set(this.dataStatus, index, !this.dataStatus[index])
+      },
+      filterBeforeEnter (el) {
+        el.style.height = 0
+      },
+      filterEnter (el) {
+        el.style.height = el.scrollHeight !== 0 ? el.scrollHeight + 'px' : 0
+      },
+      filterLeave (el) {
+        el.style.height = 0
+      },
+      initStatus () {
+        if (this.dataStatus.length) {
+          this.dataStatus = []
+        }
+        this.data.forEach(item => {
+          this.dataStatus.push(false)
+        })
+      },
+      getIcon (type) {
+        let str;
+        typeList.forEach(item => {
+          if (item.value === type) {
+            str = item.icon
+          }
+        })
+        return str
+      }
+    },
+  }
+</script>
+
+<style lang="scss">
+  .timeline-view {
+    .icon {
+      margin-left: 5px;
+    }
+
+    a, a:hover, a:active {
+      color: theme('colors.gray.darkest');
+    }
+
+    .icon {
+      transition: all .1s ease-in-out;
+    }
+
+    .active {
+      .icon {
+        transform: rotate(180deg);
+      }
+    }
+
+    .more-box {
+      overflow: hidden;
+      transition: all .3s ease-in-out;
+    }
+
+    .download {
+      display: inline-flex;
+      align-items: center;
+      color: theme('colors.blue.default');
+
+      .text {
+        margin: 0 10px 0 5px;
+      }
+
+      .icon-file {
+        width: 12px;
+        height: 12px;
+        color: theme('colors.gray.default');
+      }
+    }
+
+    .download:hover, .download:active {
+      color: theme('colors.blue.default');
+      text-decoration: underline;
+    }
+  }
+</style>

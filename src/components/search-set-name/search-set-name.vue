02a9ce19c4b2271d53203541e3f0ff73ac0598b9
liaoliao <1030219089a@gmail.com>
2019-07-17 15:32:13 +0800

    code formatting

--- src/components/search-set-name/search-set-name.vue
+++ src/components/search-set-name/search-set-name.vue
@@ -1,15 +1,16 @@
 <template>
   <el-popover
-    popper-class="set-name-popper"
+    v-model="visible"
     trigger="click"
-    v-model="visible">
+    class="set-name-view"
+    popper-class="set-name-popper">
     <h3>新常用条件名称</h3>
-    <el-input v-model="model" class="mt-1" type="text"/>
+    <el-input v-model="model" class="mt-1"/>
     <div class="text-center mt-2">
       <el-button size="small" @click="close()">取消</el-button>
       <el-button type="primary" @click="confirm(model)" size="small">确定</el-button>
     </div>
-    <p slot="reference" class="set-name-text text-blue cursor-pointer">设为常用条件</p>
+    <p slot="reference" class="content-text text-blue cursor-pointer">设为常用条件</p>
   </el-popover>
 </template>
 
@@ -55,8 +56,10 @@
 <style lang="scss">
   @import '../search-options/variables.scss';
 
-  .set-name-text {
-    line-height: $search-conditions-height;
+  .set-name-view {
+    .content-text {
+      line-height: $search-conditions-height;
+    }
   }
 
   .set-name-popper {

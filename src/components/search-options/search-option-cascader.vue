4e16d99cc89b90ec108882161c3bb1dd6e266cb4
liaoliao <1030219089a@gmail.com>
2019-07-18 10:19:58 +0800

    fix cascader组件bug备注

--- src/components/search-options/search-option-cascader.vue
+++ src/components/search-options/search-option-cascader.vue
@@ -66,6 +66,8 @@
 
     methods: {
       change () {
+        // 子集value与不同父级value相同时出现bug  需要更新到v2.11.0
+        // https://github.com/ElemeFE/element/issues/16324
         this.$nextTick(_ => {
           const index = this.index
           const values = this.model

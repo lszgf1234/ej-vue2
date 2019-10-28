1b01663f7c221f22e94d12984c1335043deb25b5
SilentDepth <silentdepth@outlook.com>
2019-10-28 14:44:21 +0800

    fix(Icon): 独立引入时 SVG Polyfill 没有正确应用

--- src/components/icon/index.js
+++ src/components/icon/index.js
@@ -1,14 +1,8 @@
-import svg4everybody from 'svg4everybody'
-
 import Icon from './icon.vue'
 
 export default {
   ...Icon,
   install: Vue => {
-    // 第一次注册组件时应用 SVG polyfill（伺候老不死的 IE 用）
-    if (!Vue.component(Icon.name)) {
-      svg4everybody()
-    }
     Vue.component(Icon.name, Icon)
   },
 }

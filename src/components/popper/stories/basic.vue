41ac72f9125290b8d01b8d6e26978f219e5b1859
liShuai <1076283698@qq.com>
2019-11-20 09:14:25 +0800

    popper 随屏弹框

--- src/components/popper/stories/basic.vue
+++ src/components/popper/stories/basic.vue
@@ -8,6 +8,16 @@
       content="这是一段内容,这是一段内容,这是一段内容,这是一段内容。">
       <el-button slot="reference">hover 激活</el-button>
     </ej-popper>
+    <ej-popper
+      placement="top-start"
+      width="200"
+      trigger="click"
+      content="这是一段内容,这是一段内容,这是一段内容,这是一段内容。">
+      <el-button slot="reference">click 激活</el-button>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci dolor facere in nisi quam sed, similique! Aliquid beatae dolores eius expedita optio placeat possimus voluptate! Alias blanditiis expedita laborum perspiciatis?
+      </div>
+    </ej-popper>
   </div>
 </template>
 

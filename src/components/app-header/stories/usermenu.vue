be678e3fb1a1b49fce5e4b08fd10f6d638b36d0b
mayanyun <nzxhdsgs@163.com>
2020-04-23 16:00:13 +0800

    引用app-usermenu时，不用带type参数

--- src/components/app-header/stories/usermenu.vue
+++ src/components/app-header/stories/usermenu.vue
@@ -2,7 +2,7 @@
   <div>
     <ej-app-header :user="user">
       <template #tray>
-        <ej-app-usermenu :user="user1" :endpoint="endpoint" :no-uc="noUc" :type="'ceb'"/>
+        <ej-app-usermenu :user="user1" :endpoint="endpoint" :no-uc="noUc"/>
       </template>
     </ej-app-header>
   </div>
